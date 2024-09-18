05acdf75ff9547460b73c52ae07bee52d8e044f0
Kaifogolik <nintendo670@gmail.com>
2024-09-18 20:27:39 +0300

    add postgresql

--- src/main/java/may/code/mash_task/Application.java
+++ src/main/java/may/code/mash_task/Application.java
@@ -2,7 +2,6 @@ package may.code.mash_task;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class Application {
@@ -10,6 +9,6 @@ public class Application {
     public static void main(String[] args) {
 
         SpringApplication
-                .run(Application.class,args);
+                .run(Application.class, args);
     }
 }
